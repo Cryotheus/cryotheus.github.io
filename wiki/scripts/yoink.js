a0da9c3fdfb0303293df6014e897c0fb85c9df06
Cryotheus <wisedthecoder@gmail.com>
2023-03-19 23:47:20 -0600

    IT'S ALIVE!!!

--- wiki/scripts/yoink.js
+++ wiki/scripts/yoink.js
@@ -1,42 +1,70 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+//https://api.github.com/repos/Cryotheus/cryotheus.github.io/contents/wiki/pages/playing
+//https://raw.githubusercontent.com/Cryotheus/cryotheus.github.io/main/wiki/pages/playing/1-Using%20Pyrition.md
 
-function yoink_pages(yoink_directory) {
-	var request = new XMLHttpRequest();
+const list_functions = {
+	developer_reference: [
+		parts => {
+			return ["r" + parts[1], parts[2], parts[2]]
+		},
+	],
+
+	developers: [parts => {
+		return ["", parts[1], parts[1]]
+	}],
+
+	playing: [parts => {
+		return ["", parts[1], parts[1]]
+	}]
+}
+
+function yoink_pages(yoink_directory, section_div) {
+	var request = new XMLHttpRequest()
 
 	request.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			const object = JSON.parse(request.responseText);
-			console.log(request.responseText);
+			const files = JSON.parse(request.responseText)
+			const function_list = list_functions[yoink_directory]
+			const ul = document.querySelectorAll('.section[yoink-directory="' + yoink_directory + '"] .level1 .yoink-pages')
+			
+			files.forEach(file => {
+				const name = file.name.slice(0, -3).replace("--", ";")
+				const raw_path = file.path
+				const matches = [...name.matchAll("[^-]*")]
+				const list_index = parseInt(matches[0])
+				const func = function_list[list_index] === undefined ? function_list[0] : function_list[list_index]
+				var parts = []
+
+				matches.forEach((part, index) => {
+					if (part[0])
+						parts.push(part[0].replace(";", "-"))
+				})
+				
+				const result = func(parts)
+				const class_attribute = result[0]
+				const display_text = result[1]
+				const search_term = result[2]
+
+				var li = document.createElement("li")
+				var a = document.createElement("a")
+				var a_text = document.createTextNode(display_text)
+
+				a.setAttribute("class", class_attribute)
+				a.setAttribute("href", "https://raw.githubusercontent.com/Cryotheus/cryotheus.github.io/main/wiki/pages/" + yoink_directory + "/" + file.name)
+				a.setAttribute("search", search_term)
+				a.appendChild(a_text)
+				li.appendChild(a)
+				ul[list_index - 1].appendChild(li)
+			});
 		}
 	};
 	
-	request.open('GET', 'https://github.com/Cryotheus/cryotheus.github.io/tree/main/wiki/pages/' + yoink_directory, false);
-	request.setRequestHeader('Accept', 'application/json');
-	request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-	request.setRequestHeader('X-GitHub-Target', 'dotcom');
-	request.send(null);
+	request.open('GET', 'https://api.github.com/repos/Cryotheus/cryotheus.github.io/contents/wiki/pages/' + yoink_directory, false)
+	request.setRequestHeader('Accept', 'application/json')
+	request.send(null)
 }
 
-document.querySelectorAll("div[yoink-directory]").forEach(element => {
-	var yoink_directory = element.getAttribute("yoink-directory");
+document.querySelectorAll("div[yoink-directory]").forEach(section_div => {
+	var yoink_directory = section_div.getAttribute("yoink-directory")
 
-	//yoink_pages(yoink_directory)
-	for (let index = 1; index <= element.childElementCount; index++) {
-		//https://api.github.com/repos/Cryotheus/cryotheus.github.io/contents/wiki/pages/playing/1
-	}
+	yoink_pages(yoink_directory, section_div)
 });
-
-const octokit = new Octokit({
-	//auth: 'YOUR-TOKEN'
-})
-//Cryotheus/cryotheus.github.io
-const result = await octokit.request('GET /repos/Cryotheus/cryotheus.github.io/contents/wiki/pages/developer_reference', {
-	owner: 'Cryotheus',
-	repo: 'cryotheus.github.io',
-	path: 'wiki/pages/developer_reference',
-	headers: {
-		'X-GitHub-Api-Version': '2022-11-28'
-	}
-})
-
-console.log(result)
